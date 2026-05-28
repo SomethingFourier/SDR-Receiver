342ae8735e77182b1bd148243d107dbc4f11b14d
Nathan Neidigh <nathan.neidigh@outlook.com>
2026-05-28 12:15:10 -0700

    Actually really 100% untracked build dir

--- firmware/build/CMakeFiles/sdr_i2s_receiver_pio_h.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/sdr_i2s_receiver_pio_h"
-  "i2s_receiver.pio.h"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/sdr_i2s_receiver_pio_h.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
