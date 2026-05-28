9d80b7376d2849f55279261b66a2d409f6f4bc2a
Nathan Neidigh <nathan.neidigh@outlook.com>
2026-05-28 12:23:05 -0700

    Actually really 100% on God I swear upon my Mother's grave and piss on all the haters have untracked the build dir.

--- build/CMakeFiles/sdr_i2s_receiver_pio_h.dir/compiler_depend.ts
+++
@@ -1,2 +0,0 @@
-# CMAKE generated file: DO NOT EDIT!
-# Timestamp file for custom commands dependencies management for sdr_i2s_receiver_pio_h.
--- build/_deps/picotool-build/data_locs.cpp
+++
@@ -1,7 +0,0 @@
-
-#include <vector>
-#include <string>
-
-std::vector<std::string> data_locs = {
-    "./","/home/neidna/SDR-Receiver/build/_deps/picotool/"
-};
