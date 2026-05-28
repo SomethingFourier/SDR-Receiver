4a951cec419619f80a748009a46fdced9b15f958
PKCubed <59457514+PKCubed@users.noreply.github.com>
2026-05-28 11:03:51 -0700

    Moved and renamed eth-test folder

---
+++ bring-up/eth-test/Attempt 3/build/CMakeFiles/rp2350_app.dir/home/pkcubed/pico-sdk/lib/lwip/src/apps/http/fs.c.o.d
@@ -0,0 +1,33 @@
+CMakeFiles/rp2350_app.dir/home/pkcubed/pico-sdk/lib/lwip/src/apps/http/fs.c.o: \
+ /home/pkcubed/pico-sdk/lib/lwip/src/apps/http/fs.c \
+ /home/pkcubed/pico-sdk/lib/lwip/src/include/lwip/apps/httpd_opts.h \
+ /home/pkcubed/pico-sdk/lib/lwip/src/include/lwip/opt.h \
+ /home/pkcubed/Documents/GitHub/SDR-Receiver/Eth-Test/Attempt\ 3/external/pico-rmii-ethernet/src/lwip/lwipopts.h \
+ /home/pkcubed/pico-sdk/lib/lwip/src/include/lwip/debug.h \
+ /home/pkcubed/pico-sdk/lib/lwip/src/include/lwip/arch.h \
+ /home/pkcubed/Documents/GitHub/SDR-Receiver/Eth-Test/Attempt\ 3/external/pico-rmii-ethernet/src/lwip/arch/cc.h \
+ /usr/include/newlib/stdio.h /usr/include/newlib/_ansi.h \
+ /usr/include/newlib/newlib.h /usr/include/newlib/_newlib_version.h \
+ /usr/include/newlib/sys/config.h /usr/include/newlib/machine/ieeefp.h \
+ /usr/include/newlib/sys/features.h /usr/include/newlib/sys/cdefs.h \
+ /usr/include/newlib/machine/_default_types.h \
+ /usr/lib/gcc/arm-none-eabi/13.2.1/include/stddef.h \
+ /usr/lib/gcc/arm-none-eabi/13.2.1/include/stdarg.h \
+ /usr/include/newlib/sys/reent.h /usr/include/newlib/_ansi.h \
+ /usr/include/newlib/sys/_types.h /usr/include/newlib/machine/_types.h \
+ /usr/include/newlib/sys/lock.h /usr/include/newlib/sys/stdio.h \
+ /usr/include/newlib/stdlib.h /usr/include/newlib/machine/stdlib.h \
+ /usr/include/newlib/alloca.h \
+ /usr/lib/gcc/arm-none-eabi/13.2.1/include/stdint.h \
+ /usr/include/newlib/inttypes.h /usr/include/newlib/sys/_intsup.h \
+ /usr/include/newlib/sys/_locale.h \
+ /usr/lib/gcc/arm-none-eabi/13.2.1/include/limits.h \
+ /usr/include/newlib/ctype.h \
+ /home/pkcubed/pico-sdk/lib/lwip/src/include/lwip/prot/iana.h \
+ /home/pkcubed/pico-sdk/lib/lwip/src/include/lwip/def.h \
+ /home/pkcubed/pico-sdk/lib/lwip/src/include/lwip/apps/fs.h \
+ /home/pkcubed/pico-sdk/lib/lwip/src/include/lwip/apps/httpd_opts.h \
+ /home/pkcubed/pico-sdk/lib/lwip/src/include/lwip/err.h \
+ /usr/include/newlib/string.h /usr/include/newlib/strings.h \
+ /usr/include/newlib/sys/string.h \
+ /home/pkcubed/pico-sdk/lib/lwip/src/apps/http/fsdata.c
---
+++ bring-up/eth-test/Attempt 3/build/external/pico-rmii-ethernet/examples/httpd/CMakeFiles/pico_rmii_ethernet_httpd.dir/home/pkcubed/pico-sdk/lib/lwip/src/netif/ppp/polarssl/arc4.c.o.d
@@ -0,0 +1,25 @@
+external/pico-rmii-ethernet/examples/httpd/CMakeFiles/pico_rmii_ethernet_httpd.dir/home/pkcubed/pico-sdk/lib/lwip/src/netif/ppp/polarssl/arc4.c.o: \
+ /home/pkcubed/pico-sdk/lib/lwip/src/netif/ppp/polarssl/arc4.c \
+ /home/pkcubed/pico-sdk/lib/lwip/src/include/netif/ppp/ppp_opts.h \
+ /home/pkcubed/pico-sdk/lib/lwip/src/include/lwip/opt.h \
+ /home/pkcubed/Documents/GitHub/SDR-Receiver/Eth-Test/Attempt\ 3/external/pico-rmii-ethernet/src/lwip/lwipopts.h \
+ /home/pkcubed/pico-sdk/lib/lwip/src/include/lwip/debug.h \
+ /home/pkcubed/pico-sdk/lib/lwip/src/include/lwip/arch.h \
+ /home/pkcubed/Documents/GitHub/SDR-Receiver/Eth-Test/Attempt\ 3/external/pico-rmii-ethernet/src/lwip/arch/cc.h \
+ /usr/include/newlib/stdio.h /usr/include/newlib/_ansi.h \
+ /usr/include/newlib/newlib.h /usr/include/newlib/_newlib_version.h \
+ /usr/include/newlib/sys/config.h /usr/include/newlib/machine/ieeefp.h \
+ /usr/include/newlib/sys/features.h /usr/include/newlib/sys/cdefs.h \
+ /usr/include/newlib/machine/_default_types.h \
+ /usr/lib/gcc/arm-none-eabi/13.2.1/include/stddef.h \
+ /usr/lib/gcc/arm-none-eabi/13.2.1/include/stdarg.h \
+ /usr/include/newlib/sys/reent.h /usr/include/newlib/_ansi.h \
+ /usr/include/newlib/sys/_types.h /usr/include/newlib/machine/_types.h \
+ /usr/include/newlib/sys/lock.h /usr/include/newlib/sys/stdio.h \
+ /usr/include/newlib/stdlib.h /usr/include/newlib/machine/stdlib.h \
+ /usr/include/newlib/alloca.h \
+ /usr/lib/gcc/arm-none-eabi/13.2.1/include/stdint.h \
+ /usr/include/newlib/inttypes.h /usr/include/newlib/sys/_intsup.h \
+ /usr/include/newlib/sys/_locale.h \
+ /usr/lib/gcc/arm-none-eabi/13.2.1/include/limits.h \
+ /usr/include/newlib/ctype.h
